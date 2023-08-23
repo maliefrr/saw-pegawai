b4e8bccd015418fd832656a654ef8375e7bcf8df
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2023-08-23 14:39:47 +0800

    add registrasi karyawan page and calon karyawan routes

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const app = require("express")()
+const express = require("express")
+const app = express()
 require("dotenv").config()
 
+// middleware
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+app.use("/register/karyawan", require("./routes/CalonKaryawanRoutes"))
 
 const port = process.env.PORT || 3000
 
 
-app.listen(() => console.log(`App is listening to port ${port}`))
+app.listen(port, () => console.log(`App is listening to port ${port}`))
