3066aaea1a96ba36845b04556a84dd1880cdead3
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2023-08-23 17:48:56 +0800

    add post routes Note: The data is not handled right now

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const app = express()
+const bodyParser = require('body-parser')
+
 require("dotenv").config()
 require("./models/index")
 
@@ -7,6 +9,7 @@ require("./models/index")
 // middleware
 app.set("view engine", "ejs")
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use("/register/karyawan", require("./routes/CalonKaryawanRoutes"))
 
--- routes/CalonKaryawanRoutes.js
+++ routes/CalonKaryawanRoutes.js
@@ -1,10 +1,15 @@
-const router = require('express').Router()
+const express = require('express')
+const router = express.Router()
 
 
 router.get("/", (req,res) => {
     res.render("registrasiKaryawan")
 })
 
+router.post("/add", (req,res) => {
+    console.log(req.body)
+    res.redirect("/register/karyawan")
+})
 
 
 module.exports = router
