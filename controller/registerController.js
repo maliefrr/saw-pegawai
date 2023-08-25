6994f735f34c80ddd7b5eec4b31070378a20270a
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2023-08-25 21:01:04 +0800

    setting flash message

--- controller/registerController.js
+++ controller/registerController.js
@@ -1,9 +1,19 @@
-const {CalonPegawai} = require("../models/calonpegawai")
-const {User} = require("../models/user")
+const {CalonPegawai} = require("../models")
+const {User} = require("../models")
 
 
 const getKaryawanRegisterPage = (req, res) => {
-    res.render("registrasiKaryawan");
+    res.render("registrasiKaryawan",{
+        error : req.flash("error"),
+        success: req.flash("success")
+    });
+}
+
+const getUserRegisterPage = (req,res) => {
+    res.render("registrasiUser",{
+        error : req.flash("error"),
+        success: req.flash("success")
+    })
 }
 
 const postKaryawanRegister = async (req, res) => {
@@ -17,18 +27,20 @@ const postKaryawanRegister = async (req, res) => {
             skills: req.body.skill
         });
 
+        req.flash("success", "Data berhasil ditambahkan ke database")
         console.log(`Data has been successfully added to database:`, data);
         res.redirect("/register/karyawan");
     } catch (error) {
         console.error("Error adding data to the database:", error);
+        req.flash("error", "Terjadi Kesalahan saat menambahkan data ke database")
         res.status(500).send("An error occurred while adding data.");
     }
 }
 
 const postUserRegister = (req,res) => {
-    console.log(req.body)
+    req.body.password !== req.body.passwordConfirmation ? req.flash("error","password dan konfirmasi password tidak sesuai") : req.flash("success", "Berhasil menambahkan data ke database")
     res.redirect("/register/user")
 }
 
 
-module.exports = {getKaryawanRegisterPage, postKaryawanRegister, postUserRegister}
+module.exports = {getKaryawanRegisterPage, postKaryawanRegister, postUserRegister, getUserRegisterPage}
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const { getKaryawanRegisterPage, postKaryawanRegister, postUserRegister } = require('../controller/registerController');
+const { getKaryawanRegisterPage, postKaryawanRegister, postUserRegister, getUserRegisterPage } = require('../controller/registerController');
 const router = express.Router();
 
 router.get("/karyawan", getKaryawanRegisterPage);
-router.get("/user", (req,res) => {
-    res.render("registrasiUser")
-})
+router.get("/user", getUserRegisterPage)
 
 
 router.post("/karyawan/add", postKaryawanRegister);
